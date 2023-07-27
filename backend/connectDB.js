21932ed7bf59e25ea2476f3ebaa3df611e44e2ea
HENRY ELUEME <juniortelli22@gmail.com>
2023-07-27 14:31:20 +0100

    mongo

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -2,16 +2,15 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@mernauth.cbzzs1r.mongodb.net/ecommerce?retryWrites=true&w=majority`;
+const connectionStr = process.env.MONGO_DB_URI;
 
 mongoose
   .connect(connectionStr, { useNewUrlParser: true })
   .then(() => console.log("connected to mongoDB"))
   .catch((err) => console.log(err));
 
-mongoose.connection.on('error', err => {
-      console.log(err)
-  })
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
 
-
-  //juniortelli22 Fx2XVk0CICgjvdPL
+//juniortelli22
